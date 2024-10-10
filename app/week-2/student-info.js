d22b0d57dcf3bf4bae7528f1255c983457ef63bc
kellyokada <kelly.okada.p@gmail.com>
2024-10-09 20:30:29 -0600

    week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Kelly Okada Pereira Aires</h1>
+            <p><Link href="https://github.com/kellyokada/cprg306-assignments">GitHub Repository</Link></p>
+        </main>
+    );
+}
